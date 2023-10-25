3d0285664f473f52bd770cb417d36bcbc7ed8447
rahulJalan23 <rahuljalan23.rj@gmail.com>
2023-10-25 12:59:59 +0530

    -a

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
   useEffect(() => {
     (async () => {
       const stocks = await getAllStocks();
-      setStocks(stocks);
+      if (stocks) setStocks(stocks);
     })();
   }, []);
 
@@ -34,25 +34,24 @@ function App() {
     <div className="text-center w-[80%] mx-auto ">
       <div className="text-3xl m-4">Stock Balance Inquiry</div>
       <div>
-        <div className="text-xl">Select Stock</div>
+        <div className="text-xl mt-20">Select Stock</div>
         <select
           onChange={(e) => {
             console.log(e.target.value);
             if (e.target.value) setSelectedStockId(e.target.value);
           }}
-          className="text-xl mt-2"
+          className="text-xl mt-2 mr-2 p-2 bg-[#B9E6FF] rounded-md border w-[70%]"
         >
           <option value={0}>None</option>
           {stocks.map((stock) => {
             return <option value={stock._id}>{stock.name}</option>;
           })}
         </select>
-        <button>Submit</button>
       </div>
-      <div className="flex justify-center">
-        <div className="text-xl m-2">Balance:</div>
-        <div className="m-2 text-xl">
-          {selectedStockId && balance ? <>{balance}</> : <>Select Stock</>}
+      <div className="justify-center mt-32">
+        <div className="text-2xl m-2">Balance</div>
+        <div className="m-10 text-6xl">
+          {selectedStockId && balance ? <>{balance}</> : <></>}
         </div>
       </div>
     </div>
@@ -60,3 +59,5 @@ function App() {
 }
 
 export default App;
+
+// https://coolors.co/7e6c6c-f87575-ffa9a3-b9e6ff-5c95ff
